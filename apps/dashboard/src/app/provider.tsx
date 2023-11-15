049ca61b48fc1846b158a0f7eb874bb65f830d99
João Martins <jvmartinsdev@gmail.com>
2023-11-15 16:06:20 -0300

    feat(dashboard): add theme provider

--- apps/dashboard/src/app/provider.tsx
+++ apps/dashboard/src/app/provider.tsx
@@ -1,11 +1,21 @@
-'use client'
+"use client";
 
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react";
 
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "ui/components/ui/theme-provider";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function Provider({ children }: { children: ReactNode }) {
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ThemeProvider>
+  );
 }
