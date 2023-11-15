74beb433cb3874882a2a6977ea2f137c4e6bd0f5
João Martins <jvmartinsdev@gmail.com>
2023-11-15 15:54:15 -0300

    chore(dashboard): import configs from ui package

--- apps/dashboard/src/app/layout.tsx
+++ apps/dashboard/src/app/layout.tsx
@@ -1,15 +1,15 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
 
-import Provider from './provider'
-import 'tailwindcss/tailwind.css'
+import Provider from "./provider";
+import "@ui/styles/globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <html>
-            <head></head>
-            <body>
-                <Provider>{children}</Provider>
-            </body>
-        </html>
-    )
+  return (
+    <html>
+      <head></head>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
+    </html>
+  );
 }
